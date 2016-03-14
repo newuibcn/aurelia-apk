9609ac8d766ba2a018fc3f822fed725c9dbeeeee
Alex <avizcaino@newuibcn.com>
2016-03-14 17:27:36 +0100

    git deploy gulp tasks --server-side

--- base/server/index.js
+++ base/server/index.js
@@ -3,9 +3,39 @@
  */
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var prompt = require('gulp-prompt');
+var runSequence = require('run-sequence');
+var promptInput;
+
+gulp.task('fill-data', function(){
+    return gulp.src(paths.exportSrv)
+        .pipe(prompt.prompt([{
+            type: 'input',
+            name: 'username',
+            message: 'Enter git username'
+        },{
+            type: 'input',
+            name: 'email',
+            message: 'Enter git email'
+        },{
+            type: 'password',
+            name: 'password',
+            message: 'Enter git password'
+        }], function(res){
+            promptInput = res;
+        }));
+});
+
+gulp.task('config', function(cb){
+    exec('cd export && git config user.email ' + promptInput.email, function(err, stdout, stderr){
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
 
 gulp.task('pull', function(cb){
-    exec('git pull', function(err, stdout, stderr){
+    exec('git fetch --all && git reset --hard origin/master', function(err, stdout, stderr){
         console.log(stdout);
         console.log(stderr);
         cb(err);
@@ -13,12 +43,37 @@ gulp.task('pull', function(cb){
 });
 
 gulp.task('test', function(cb){
-    exec('git checkout test && git reset --hard origin/master', function(err, stdout, stderr){
+    exec('git checkout test && git rebase master && git rebase --skip && git push -uf origin test', function(err, stdout, stderr){
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
 });
+
+gulp.task('prompt-tag', function(){
+    return gulp.src('./')
+        .pipe(prompt.prompt([{
+            type: 'input',
+            name: 'tagVersion',
+            message: 'Enter tag version'
+        },{
+            type: 'input',
+            name: 'tagAnnotation',
+            message: 'Enter tag annotation'
+        }], function(res){
+            promptInput['tag-version'] = res.tagVersion;
+            promptInput['tag-annotation'] = res.tagAnnotation;
+        }))
+})
+
+gulp.task('tag', ['prompt-tag'], function(cb){
+    exec('git tag -a ' + promptInput.tagVersion + ' -m ' + promptInput.tagAnnotation +
+        'git push origin ' + promptInput.tagVersion, function(err, stdout, stderr){
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+})
 
 gulp.task('release', function(cb){
     exec('git checkout release && git reset --hard origin/test', function(err, stdout, stderr){
