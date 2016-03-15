3a8b904cd325307bea0453b06e08e5a27b4b86c1
Alex <avizcaino@newuibcn.com>
2016-03-15 12:04:41 +0100

    git deploy gulp tasks --server-side (final)

--- base/server/index.js
+++ base/server/index.js
@@ -7,7 +7,7 @@ var prompt = require('gulp-prompt');
 var promptInput;
 
 gulp.task('fill-data', function(){
-    return gulp.src(paths.exportSrv)
+    return gulp.src('./')
         .pipe(prompt.prompt([{
             type: 'input',
             name: 'username',
@@ -27,6 +27,14 @@ gulp.task('fill-data', function(){
 
 gulp.task('config', function(cb){
     exec('cd export && git config user.email ' + promptInput.email, function(err, stdout, stderr){
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
+gulp.task('init', function(cb){
+    exec('git remote add origin https://' + promptInput.username + ':' + promptInput.password + '@github.com/' + promptInput.username + '/test.git', function(err, stdout, stderr){
         console.log(stdout);
         console.log(stderr);
         cb(err);
@@ -58,8 +66,8 @@ gulp.task('prompt-tag', function(){
 })
 
 gulp.task('tag', ['prompt-tag'], function(cb){
-    exec('git tag -a ' + promptInput.tagVersion + ' -m ' + promptInput.tagAnnotation +
-        'git push origin ' + promptInput.tagVersion, function(err, stdout, stderr){
+    exec('git tag -a ' + promptInput.tagVersion + ' -m "' + promptInput.tagAnnotation +
+        '" && git push origin ' + promptInput.tagVersion, function(err, stdout, stderr){
         console.log(stdout);
         console.log(stderr);
         cb(err);
@@ -67,7 +75,7 @@ gulp.task('tag', ['prompt-tag'], function(cb){
 });
 
 gulp.task('test', ['fill-data'], function(cb){
-    exec('git push origin --delete test && git checkout -b test && git push origin test', function(err, stdout, stderr){
+    exec('git remote rm origin && git remote add origin https://' + promptInput.username + ':' + promptInput.password + '@github.com/' + promptInput.username + '/test.git && git push origin --delete test && git checkout -b test && git push origin test', function(err, stdout, stderr){
         console.log(stdout);
         console.log(stderr);
         cb(err);
@@ -75,7 +83,7 @@ gulp.task('test', ['fill-data'], function(cb){
 });
 
 gulp.task('release', ['fill-data'], function(cb){
-    exec('git push origin --delete release && git checkout -b release && git push origin release', function(err, stdout, stderr){
+    exec('git remote rm origin && git remote add origin https://' + promptInput.username + ':' + promptInput.password + '@github.com/' + promptInput.username + '/test.git && git push origin --delete release && git checkout -b release && git push origin release', function(err, stdout, stderr){
         console.log(stdout);
         console.log(stderr);
         cb(err);
